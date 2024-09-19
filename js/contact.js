ba1b8ce13e139f9707de797dc1fac15e61575ccd
Felix Oppermann <felixoppermann@freenet.de>
2024-09-19 20:32:21 +0200

    styling add new contact button

--- js/contact.js
+++ js/contact.js
@@ -16,7 +16,7 @@ async function getContacts() {
 async function renderContacts() {
     let allContacts = Object.values(await getContacts());
     allContacts.sort((a, b) => a.name.localeCompare(b.name));
-    document.getElementById('contacts').innerHTML = '';
+    document.getElementById('contacts').innerHTML = addNewContactsContent();
     allContacts.forEach((contact, index) =>{
         checkForExistingLetter(contact, index);
         document.getElementById('contacts').innerHTML += contactContent(index); 
@@ -75,6 +75,15 @@ function nameHeaderContent(letter) {
         <div class="seperator">
             <div class="line">
             </div>
+        </div>
+    `
+}
+
+
+function addNewContactsContent () {
+    return `
+        <div class="add-new-contacts">
+            <button onclick="addNewContact()">Add new contact<img src="../assets/icons/person_add.png"></img></button>
         </div>
     `
 }
