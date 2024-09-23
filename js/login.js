2abb1d8d68b8317edda6ddfc33d76f00aab3fd0a
bartekkuczek <bartosz.kuczek4@gmail.com>
2024-09-23 21:25:26 +0200

    added logo animation in login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function init() {
+    setTimeout(playLogoAnimation, 250);
+}
+
+function playLogoAnimation() {
+    document.getElementById("logo").classList.remove("loading-logo-animation");
+    document.getElementById("logo").classList.add("loading-logo-animation");
+    document.getElementById("loading-overlay-id").classList.add("loading-overlay-animation");
+}
