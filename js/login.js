506e6d35ccb5e4eed0355a61d5b025b80aa470e2
bartekkuczek <bartosz.kuczek4@gmail.com>
2024-09-24 13:16:52 +0200

    fixed some js by adding some functions + styling

--- js/login.js
+++ js/login.js
@@ -9,6 +9,43 @@ function playLogoAnimation() {
     document.getElementById("loading-overlay-id").classList.add("loading-overlay-animation");
 }
 
+function login() {
+    document.getElementById("whole-body-id").innerHTML = renderLogIn();
+    document.getElementById("sign-up-top-right-id").classList.remove("d-none");
+    renderLogIn();
+}
+
+function signUp() {
+    document.getElementById("sign-up-top-right-id").classList.add("d-none");
+    document.getElementById("whole-body-id").innerHTML = renderSignUp();
+}
+
+function renderSignUp() {
+    return `
+       <div class="sign-up-mask">
+       <div class="sign-up-title">
+            <img onclick="login()" src="/assets/icons/back.png">
+            <b class="login-signup-title">Sign Up</b>
+       </div>
+        <div class="login-seperator"></div>
+        <div class="login-form">
+            <form>
+                <input class="name-input" type="text" placeholder="Name" required> 
+                <input class="email-input" type="email" placeholder="Email" required> 
+                <input class="password-input" type="text" placeholder="Password" required>
+                <input class="password-input" type="text" placeholder="Confirm Password" required>
+                <div class="check-box" style="padding-left: 0px; justify-content: center;">
+                    <div onclick="rememberMe()" class="remember-true"></div>
+                    <span class="sign-up-check-box">I accept the <a class="sign-up-check-box-privacy-policy" href="privacy-policy.html">Privacy Policy</a></span>
+                </div>
+                <div class="what-kind-of-login">
+                    <a class="just-login" href="">Sign Up</a>
+                </div>
+            </form>
+        </div>
+    `;
+}
+
 function renderLogIn() {
     return `
     <div class="login-mask">
@@ -18,51 +55,16 @@ function renderLogIn() {
                 <form>
                     <input class="email-input" type="email" placeholder="Email" required> 
                     <input class="password-input" type="text" placeholder="Password" required>
-                    <div class="remember">
+                    <div class="check-box">
                         <div onclick="rememberMe()" class="remember-true"></div>
                         <span>Remember me</span>
                     </div>
                     <div class="what-kind-of-login">
                         <a class="just-login" href="">Log in</a>
-                        <a class="guest-login" style="color: black;" href="">Guest Log in</a>
+                        <a class="guest-login" style="color: black;" href="board.html">Guest Log in</a>
                     </div>
                 </form>
             </div>
     </div>
-    `;
-}
-
-function signUp() {
-    document.getElementById("sign-up-top-right-id").classList.add("d-none");
-    document.getElementById("whole-body-id").innerHTML = renderSignUp();
-    document.querySelector('.sign-up-title img').addEventListener('click', renderLogIn);
-
-}
-
-function renderSignUp() {
-
-    return `
-       <div class="sign-up-mask">
-       <div class="sign-up-title">
-            <img onclick="renderLogIn()" src="/assets/icons/back.png">
-            <b class="login-signup-title">Sign Up</b>
-       </div>
-
-        <div class="login-seperator"></div>
-        <div class="login-form">
-            <form>
-                <input class="name-input" type="text" placeholder="Name" required> 
-                <input class="email-input" type="email" placeholder="Email" required> 
-                <input class="password-input" type="text" placeholder="Password" required>
-                <input class="password-input" type="text" placeholder="Confirm Password" required>
-                <div class="remember">
-                    <div onclick="rememberMe()" class="remember-true"></div>
-                    <span>I accept the Privacy Policy</span>
-                </div>
-                <div class="what-kind-of-login">
-                    <a class="just-login" href="">Sign Up</a>
-                </div>
-            </form>
-        </div>
     `;
 }
