d6b8ba2b58eb0e7136acd94f9e07f095b6dcb81d
Felix Oppermann <felixoppermann@freenet.de>
2024-09-29 12:01:09 +0200

    added editContact function

--- js/contact.js
+++ js/contact.js
@@ -80,6 +80,7 @@ async function setContactInformation(index) {
     document.getElementById('phoneNumber').innerText = contacts[index].phone
     document.getElementById('initialsArticle').innerText = generateInitials(contacts[index].name) 
     document.getElementById('delContact').onclick = () => {deleteContact(contacts[index].id)}
+    document.getElementById('editContact').onclick = () => {editContact(contacts[index].id)}
     setSelectedContactBackground(index);   
 }
 
@@ -102,7 +103,9 @@ function setSelectedContactBackground(index) {
 
 
 function openAndCloseAddContact() {
-    let modal = document.getElementById('addContact');  
+    let modal = document.getElementById('addContact');
+    modal.innerHTML = '';
+    modal.innerHTML = addContactCardContent();  
     modal.style.display ='flex';
     document.getElementById('modalBackground').style.display = 'flex';
     checkIfAnimationActive()
@@ -165,6 +168,14 @@ async function deleteContact(id) {
 }
 
 
+async function editContact(index) {
+    let content = document.getElementById('addContact');
+    openAndCloseAddContact();
+    content.innerHTML = '';
+    content.innerHTML = editContactCardContent(index);
+}
+
+
 function contactContent(index) {
     return `
         <div onclick ="openContact('${index}')" class="single-contact">
@@ -197,6 +208,70 @@ function addNewContactsContent () {
     return `
         <div class="add-new-contacts">
             <button onclick="openAndCloseAddContact()">Add new contact<img src="../assets/icons/person_add.png"></img></button>
+        </div>
+    `
+}
+
+function addContactCardContent(){
+    return ` 
+        <div class="modal-header-logo-left">
+          <div class="logo-modal">
+            <img src="../assets/img/logo-white.png" alt="logo-white">
+          </div>
+          <div class="header-modal">
+            <h1>Add Contact</h1>
+            <h2>Tasks are better with a team!</h2>
+            <div class="header-seperator"></div>
+          </div>
+        </div>
+        <div class="add-contact-modal-right">   
+          <div class="add-contact-modal-person">
+            <img src="../assets/icons/add-contact-person.png" alt="profile-add-contact">
+          </div>
+          <div class="input-field-right">
+              <button onclick="openAndCloseAddContact()" id="closeAddContact"><img src="../assets/icons/close.png" alt="close-button"></button>
+              <form onsubmit="createContact()" class="input-fields" action="">
+                  <input placeholder="Name" id="inputContactName" type="text" required>
+                  <input placeholder="Email" id="inputMailAddress" type="email" required>
+                  <input placeholder="Phone" id="inputPhoneNumber" type="text" required>
+                <div class="add-contact-button-bottom">
+                  <button onclick="openAndCloseAddContact()" >Cancel</button>
+                  <button type="submit" >Create contact</button>
+                </div>
+              </form>
+          </div>
+        </div>
+    `
+}
+
+function editContactCardContent(index){
+    return `
+        <div class="modal-header-logo-left">
+          <div class="logo-modal">
+            <img src="../assets/img/logo-white.png" alt="logo-white">
+          </div>
+          <div class="header-modal">
+            <h1>Edit Contact</h1>
+            <h2></h2>
+            <div class="header-seperator"></div>
+          </div>
+        </div>
+        <div class="add-contact-modal-right">   
+          <div class="add-contact-modal-person">
+            <img src="../assets/icons/add-contact-person.png" alt="profile-add-contact">
+          </div>
+          <div class="input-field-right">
+              <button onclick="openAndCloseAddContact()" id="closeAddContact"><img src="../assets/icons/close.png" alt="close-button"></button>
+              <form onsubmit="createContact()" class="input-fields" action="">
+                  <input placeholder="Name" id="inputContactName" type="text" required>
+                  <input placeholder="Email" id="inputMailAddress" type="email" required>
+                  <input placeholder="Phone" id="inputPhoneNumber" type="text" required>
+                <div class="add-contact-button-bottom">
+                  <button onclick="openAndCloseAddContact()" >Cancel</button>
+                  <button type="submit" >Create contact</button>
+                </div>
+              </form>
+          </div>
         </div>
     `
 }
