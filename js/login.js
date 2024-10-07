241a15e532a7c1d917ed85d0d18543862e53d36b
bartekkuczek <bartosz.kuczek4@gmail.com>
2024-10-07 16:45:08 +0200

    finished working on the logo animation in responsive

--- js/login.js
+++ js/login.js
@@ -10,6 +10,8 @@ function init() {
 function playLogoAnimation() {
     document.getElementById("logo").classList.remove("loading-logo-animation");
     document.getElementById("logo").classList.add("loading-logo-animation");
+    document.getElementById("logo-white").classList.remove("loading-logo-animation");
+    document.getElementById("logo-white").classList.add("loading-logo-animation");
     document.getElementById("loading-overlay-id").classList.add("loading-overlay-animation");
 }
 
