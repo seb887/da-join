39c40eb90d6713c33b2ae98996a59b9f5334cdaa
Felix Oppermann <felixoppermann@freenet.de>
2024-10-08 20:04:07 +0200

    editing functions add-tasks

--- js/add-task.js
+++ js/add-task.js
@@ -241,8 +241,7 @@ function dropDownContacts() {
   }else{
     contactList.style.display = 'flex'
     document.getElementById('searchContact').placeholder = '';
-    document.getElementById('arrowAssignTo').src =
-      '../assets/icons/arrow-up.png';
+    document.getElementById('arrowAssignTo').src ='../assets/icons/arrow-up.png';
   }
 }
 
