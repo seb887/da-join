6d678e504da57154e9e4ca6d79e1879bde8f8fb8
Felix Oppermann <felixoppermann@freenet.de>
2024-11-11 14:18:34 +0100

    updated clearInput function for assigned input field

--- js/add-task.js
+++ js/add-task.js
@@ -118,11 +118,11 @@ function clearInputs() {
   inputSubtask.value = '';
   subtasks = [];
   assignedContactList.innerHTML = '';
+  document.getElementById('searchContact').value = '';
   listContactsToAssignedTo();
   renderSubtasksList();
   setPrio('medium');
   hideSubtaskIcons();
-
 }
 
 // PRIO
